48ce2e117cb8309c5fc60389f0d10da21d48ade1
潘江林 <panjianglin@panjianglindeMacBook-Air.local>
2018-07-17 17:12:37 +0800

    优化

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,37 +3,39 @@
     <h3>{{ msg }}</h3>
     <h2>基础组件</h2>
     <h3>按钮</h3>
+    <!-- <h4>圆角按钮</h4> -->
     <xm-button>普通</xm-button>
-    <xm-button type="primary" @click="btnClick" icon="xm__icon--checked">主要</xm-button>
-    <xm-button type="error" icon="xm__icon--checked"></xm-button>
+    <xm-button type="primary" @click="btnClick" icon="xm__icon--refresh">主要</xm-button>
+    <xm-button type="error" icon="xm__icon--likefill"></xm-button>
     <xm-button type="warning" disabled>禁止</xm-button>
     <xm-button plain>简约</xm-button>
-    <xm-button type="text" icon="xm__icon--checked">文字</xm-button>
-    <xm-button loading type="success">loading状态</xm-button>
-    <xm-button bg-color="#fc0" color="#e0439a" border-color="#fc0">自定义颜色</xm-button>
-    <br/><br/>
+    <!-- <h4>无圆角按钮</h4> -->
+    <br/>
+    <br/>
+    <xm-button loading no-radius type="primary">loading状态</xm-button>
+    <xm-button no-radius bg-color="#f56c6c" color="#fff" border-color="#f56c6c">自定义颜色</xm-button>
     <xm-button no-radius>无圆角</xm-button>
+    <xm-button no-radius type="text" icon="xm__icon--checked">文字</xm-button>
+    <!-- <h4>圆形按钮</h4> -->
+    <br/>
+    <br/>
     <xm-button circle type="primary" @click="btnClick" icon="xm__icon--refresh"></xm-button>
     <xm-button circle type="success" icon="xm__icon--checked"></xm-button>
     <xm-button circle @click="btnClick" icon="xm__icon--search"></xm-button>
     <xm-button circle type="error"  icon="xm__icon--delete"></xm-button>
     <xm-button circle type="warning" icon="xm__icon--lock"></xm-button>
-    <br/><br/>
+    <!-- <h4>long按钮</h4> -->
+    <br/>
+    <br/>
     <xm-button round long>100%宽圆角</xm-button>
-    <br/><br/>
+    <br/>
+    <br/>
+    <!-- <h4>组合按钮</h4> -->
     <xm-button-group class="btn__group">
       <xm-button type="warning">警告</xm-button>
       <xm-button type="primary" @click="btnClick" icon="xm__icon--checked">主要</xm-button>
       <xm-button type="success" icon="xm__icon--loading">成功</xm-button>
     </xm-button-group>
-    <h3>标签</h3>
-    <xm-tag>default</xm-tag>
-    <xm-tag v-if="closeFlag" closable @close="close">close</xm-tag>
-    <xm-tag type="primary">primary</xm-tag>
-    <xm-tag type="success">success</xm-tag>
-    <xm-tag type="warning">warning</xm-tag>
-    <xm-tag type="error">error</xm-tag>
-    <xm-tag round bg-color="#fc0" color="#e0439a" border-color="#fc0">自定义颜色</xm-tag>
     <h3>单选按钮</h3>
     <xm-radio v-model="radio" :value="1">备选项1</xm-radio>
     <xm-radio v-model="radio" :value="2">备选项2</xm-radio>
@@ -44,6 +46,14 @@
     <xm-checkbox v-model="checkbox" value="2" name="1" @change="input">火车</xm-checkbox>
     <xm-checkbox v-model="checkbox" value="3" name="1" @change="input" disabled>飞机--disabled</xm-checkbox>
     <xm-checkbox v-model="checkbox" value="4" name="1" @change="input">单车</xm-checkbox>
+    <h3>标签</h3>
+    <xm-tag>default</xm-tag>
+    <xm-tag v-if="closeFlag" closable @close="close">close</xm-tag>
+    <xm-tag type="primary">primary</xm-tag>
+    <xm-tag type="success">success</xm-tag>
+    <xm-tag type="warning">warning</xm-tag>
+    <xm-tag type="error">error</xm-tag>
+    <xm-tag round bg-color="#fc0" color="#e0439a" border-color="#fc0">自定义颜色</xm-tag>
     <h3>文本框</h3>
     <div class="row">
       <div class="col-md-12">
